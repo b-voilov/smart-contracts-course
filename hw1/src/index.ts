8a4e586455509dfc7609ab1eccc7f5cb97fe45c1
Bohdan Voilov <40231933+b-voilov@users.noreply.github.com>
2025-03-31 02:53:02 +0300

    add(notes): crud with persistence

--- hw1/src/index.ts
+++ hw1/src/index.ts
@@ -1,15 +1,39 @@
-import { IDL, query, update } from 'azle';
+import { IDL, query, update, StableBTreeMap, time, } from 'azle';
+import { Some, None } from 'azle/experimental';
+const NoteIDL = IDL.Record({
+    title: IDL.Text,
+    content: IDL.Text,
+})
 
+type Note = {
+    title: string,
+    content: string,
+}
+const notes = new StableBTreeMap<bigint, Note>(0);
 export default class {
-    message: string = 'Hello world!';
 
-    @query([], IDL.Text)
-    getMessage(): string {
-        return this.message;
+    @query([], IDL.Vec(IDL.Tuple(IDL.Nat64, NoteIDL)))
+    getNotes(): [bigint, Note][] {
+        return notes.items();
     }
 
-    @update([IDL.Text])
-    setMessage(message: string): void {
-        this.message = message;
+    @query([IDL.Nat64], IDL.Opt(IDL.Text))
+    getNoteById(id: bigint): [string] | [] {
+        const rec = notes.get(id)
+        return rec ? [rec.title] : []
+    }
+
+
+    @update([NoteIDL], IDL.Nat64)
+    addNote(note: Note): bigint {
+        const id = time()
+        notes.insert(id, note);
+        return id;
+    }
+
+    @update([IDL.Nat64], IDL.Opt(NoteIDL))
+    deleteNoteById(id: bigint): [Note] | [] {
+        const note = notes.remove(id);
+        return note ? [note] : []
     }
 }
